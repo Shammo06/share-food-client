88f4999aefe1da5acc135797d741151e8999c97a
Shammo06 <mrshammo2018@gmail.com>
2023-11-06 12:34:49 +0600

    add error page

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -4,7 +4,7 @@ import error from '../../../public/error.jpg'
 const ErrorPage = () => {
     return (
         <div className="text-center">
-            <img className="w-2/4 py-10 mx-auto" src={error} alt="" />   
+            <img className="w-2/4 py-10 mx-auto" src={error} />   
             <Link to='/'><button className="btn btn-warning w-40">Go Home</button></Link>         
         </div>
     );
