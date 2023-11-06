0b7b081fa1d4560d2d34a7a3349d5e9b9b087684
Shammo06 <mrshammo2018@gmail.com>
2023-11-06 12:47:55 +0600

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Footer from './Component/Footer'
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
           <Outlet></Outlet>
+          <Footer></Footer>
       </div>
     </>
   )
